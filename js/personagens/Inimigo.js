5e57dce0e2897581cc47ef19360a0867e40f0f1f
jeanjjp <magaiver_10@hotmail.com>
2021-03-01 00:29:41 -0300

    ïnicio de imputs dos inimigos

--- js/personagens/Inimigo.js
+++ js/personagens/Inimigo.js
@@ -14,6 +14,12 @@ class Inimigo {
 		this.inimigoSelecionado = false;
 		this.camada = camada;
 		this.img = img;
+		this.mostrarStatus = false;
+
+		//criar objeto para inputXs
+		this.inputX = document.createElement('input');
+		this.inputY = document.createElement('input');
+
 	}
 
 
@@ -47,12 +53,45 @@ class Inimigo {
 
 		if (Util.colide(this.posX, mouseX, this.posY, mouseY, this.tamX, 0, this.tamY, 0) && click && editorON && blocoSolto) {
 			this.inimigoSelecionado = true;
+
+			if (this.mostrarStatus) {
+				this.mostrarStatus = false;
+				this.inputX.remove();
+				this.inputY.remove();
+				this.velX = parseInt(this.inputX.value);
+				this.velY = parseInt(this.inputY.value);
+			} else {
+				this.mostrarStatus = true;
+				
+				this.inputX.value = this.velX.toFixed(3);
+				this.inputY.value = this.velY.toFixed(3);
+				
+				this.inputX.style.width = 50 + "px";
+				this.inputX.type = 'number';
+				this.inputX.style.position = 'absolute';
+				this.inputX.style.left = (this.posX + 62) + 'px';
+				this.inputX.style.top = (this.posY + 22) + 'px';
+				
+				this.inputY.style.width = 50 + "px";
+				this.inputY.type = 'number';
+				this.inputY.style.position = 'absolute';
+				this.inputY.style.left = (this.posX + 62) + 'px';
+				this.inputY.style.top = (this.posY + 62) + 'px';
+
+				document.body.appendChild(this.inputX);
+				document.body.appendChild(this.inputY);
+				this.inputX.focus();
+
+			}
+
 			blocoSolto = false;
 		}
 
 		if (this.inimigoSelecionado) {
-			this.posX = mouseX - this.tamX / 2;
-			this.posY = mouseY - this.tamY / 2;
+			if (this.mostrarStatus === false) {
+				this.posX = mouseX - this.tamX / 2;
+				this.posY = mouseY - this.tamY / 2;
+			}
 
 			if (!click) {
 				this.inimigoSelecionado = false;
@@ -70,6 +109,17 @@ class Inimigo {
 			contexto.beginPath();
 			contexto.fill();
 			contexto.closePath();
+			if (this.mostrarStatus && editorON) {
+				contexto.beginPath();
+				contexto.rect(this.posX + 50, this.posY, 75, 100);
+				contexto.fillStyle = "BLUE";
+				contexto.fill();
+				contexto.fillStyle = "WHITE"
+				contexto.font = "normal 8pt Arial";
+				contexto.fillText("Vel.X = " + this.velX.toFixed(3), this.posX + 52, this.posY + 12);
+				contexto.fillText("Vel.Y = " + this.velY.toFixed(3), this.posX + 52, this.posY + 52);
+				contexto.closePath();
+			}
 		} else {
 			contexto.beginPath();
 			contexto.rect(this.posX, this.posY, this.tamX, this.tamY);
